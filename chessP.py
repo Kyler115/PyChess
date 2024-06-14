c3c2aa37dca75a21cc6f8d4d9e5e7b55006f52d6
Kyler VanCamp <105409263+Kyler115@users.noreply.github.com>
2024-06-14 15:36:40 -0500

    button
    butt

--- chessP.py
+++ chessP.py
@@ -2,8 +2,6 @@ import sys
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QWidget
 from PyQt5.QtCore import QSize, Qt, QDir
-
-
 from PyQt5.QtGui import QPalette, QColor, QIcon
 ###############################################################################
 class Color(QWidget): 
@@ -19,21 +17,32 @@ class Color(QWidget):
 class Button(QPushButton):
     def __init__(self,buttonId):
         super().__init__()      
-        self.buttonId = buttonId    
+        self.buttonId = buttonId 
+        self.typeB = ""
     
     def getBID(self):
         return self.buttonId
-###############################################################################
-class MainWindow(QMainWindow):       
     
+    def setBID(self,iD):
+        self.buttonId = iD
+        
+    def setType(self,typeB):
+        self.typeB = typeB
+        
+    def getType(self):
+        return self.typeB
+###############################################################################
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow,self).__init__()
-        self.setWindowTitle("bruh")
+        self.setWindowTitle("Chesssssssssssssssse")
         layout = QGridLayout()
         self.buttons = []
+        self.flag6 = False
+        self.stack = []
         x,self.y,self.z = 0,0,0
-        matrix = np.array([[1,2,3,4,5,3,2,1],[6,6,6,6,6,6,6,6],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[6,6,6,6,6,6,6,6],[1,2,3,4,5,3,2,1]])
-        print(matrix)
+        ## matrix = np.array([[1,2,3,4,5,3,2,1],[6,6,6,6,6,6,6,6],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[6,6,6,6,6,6,6,6],[1,2,3,4,5,3,2,1]])
+        ##print(matrix)
         ##KEY##
         ## 1 = ROOKS
         ## 2 = KNIGHTS 
@@ -45,29 +54,29 @@ class MainWindow(QMainWindow):
         
         
         rookD = "rook.png"
-        absPRD = QDir().absoluteFilePath(rookD)
+        self.absPRD = QDir().absoluteFilePath(rookD)
         bishopD = "bishop.png"
-        absPBD = QDir().absoluteFilePath(bishopD)
+        self.absPBD = QDir().absoluteFilePath(bishopD)
         knightD = "knight.png"
-        absPKD = QDir().absoluteFilePath(knightD)
+        self.absPKD = QDir().absoluteFilePath(knightD)
         kingD = "king.png"
-        absPKKD = QDir().absoluteFilePath(kingD)
+        self.absPKKD = QDir().absoluteFilePath(kingD)
         queenD = "queen.png"
-        absPQD = QDir().absoluteFilePath(queenD)
+        self.absPQD = QDir().absoluteFilePath(queenD)
         pawnD = "pawn.png"
-        absPPD = QDir().absoluteFilePath(pawnD)
+        self.absPPD = QDir().absoluteFilePath(pawnD)
         pawnW = "pawnW.png"
-        absPPW = QDir().absoluteFilePath(pawnW)
+        self.absPPW = QDir().absoluteFilePath(pawnW)
         rookW = "rookW.png"
-        absPRW = QDir().absoluteFilePath(rookW)
+        self.absPRW = QDir().absoluteFilePath(rookW)
         knightW = "knightW.png"
-        absPKW = QDir().absoluteFilePath(knightW)
+        self.absPKW = QDir().absoluteFilePath(knightW)
         bishopW = "bishopW.png"
-        absPBW = QDir().absoluteFilePath(bishopW)
+        self.absPBW = QDir().absoluteFilePath(bishopW)
         queenW = "queenW.png"
-        absPQW = QDir().absoluteFilePath(queenW)
+        self.absPQW = QDir().absoluteFilePath(queenW)
         kingW = "kingW.png"
-        absPKKW = QDir().absoluteFilePath(kingW)
+        self.absPKKW = QDir().absoluteFilePath(kingW)
         
         for x in range(8):
             for y in range(8):
@@ -88,52 +97,84 @@ class MainWindow(QMainWindow):
                     self.z = self.z+1
                     
                 if(self.z == 1):
-                    buttonT.setIcon(QIcon(absPRD))
+                    buttonT.setIcon(QIcon(self.absPRD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("rook")
                 elif(self.z == 3):
-                    buttonT.setIcon(QIcon(absPBD))
+                    buttonT.setIcon(QIcon(self.absPBD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("bishop")
                 elif(self.z == 2):
-                    buttonT.setIcon(QIcon(absPKD))
+                    buttonT.setIcon(QIcon(self.absPKD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("knight")
                 elif(self.z == 4):
-                    buttonT.setIcon(QIcon(absPQD))
+                    buttonT.setIcon(QIcon(self.absPQD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("queen")
                 elif(self.z == 5):
-                    buttonT.setIcon(QIcon(absPKKD))
+                    buttonT.setIcon(QIcon(self.absPKKD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("king")
                 elif(self.z == 6):
-                    buttonT.setIcon(QIcon(absPBD))
+                    buttonT.setIcon(QIcon(self.absPBD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("bishop")
                 elif(self.z == 7):
-                    buttonT.setIcon(QIcon(absPKD))
+                    buttonT.setIcon(QIcon(self.absPKD))
                     buttonT.setIconSize(QSize(100,100))  
+                    buttonT.setBID(self.z)
+                    buttonT.setType("knight")
                 elif(self.z == 8):
-                    buttonT.setIcon(QIcon(absPRD))
+                    buttonT.setIcon(QIcon(self.absPRD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("rook")
                 elif(self.z in range(17)):
-                    buttonT.setIcon(QIcon(absPPD))
+                    buttonT.setIcon(QIcon(self.absPPD))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("pawn")
+                elif(self.z < 49):
+                    buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
                 elif(self.z >= 49 and self.z in range(57)):
-                    buttonT.setIcon(QIcon(absPPW))
+                    buttonT.setIcon(QIcon(self.absPPW))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("pawn")
                 elif(self.z == 57 or self.z == 64):
-                    buttonT.setIcon(QIcon(absPRW))
+                    buttonT.setIcon(QIcon(self.absPRW))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("rook")
                 elif(self.z == 58 or self.z == 63):
-                    buttonT.setIcon(QIcon(absPKW))
+                    buttonT.setIcon(QIcon(self.absPKW))
                     buttonT.setIconSize(QSize(100,100))
+                    buttonT.setBID(self.z)
+                    buttonT.setType("knight")
                 elif(self.z == 59 or self.z == 62):
-                    buttonT.setIcon(QIcon(absPBW))
-                    buttonT.setIconSize(QSize(100,100))      
+                    buttonT.setIcon(QIcon(self.absPBW))
+                    buttonT.setIconSize(QSize(100,100))  
+                    buttonT.setBID(self.z)
+                    buttonT.setType("bishop")
                 elif(self.z == 60):
-                    buttonT.setIcon(QIcon(absPQW))
+                    buttonT.setIcon(QIcon(self.absPQW))
                     buttonT.setIconSize(QSize(100,100)) 
+                    buttonT.setBID(self.z)
+                    buttonT.setType("queen")
                 elif(self.z == 61):
-                    buttonT.setIcon(QIcon(absPKKW))
+                    buttonT.setIcon(QIcon(self.absPKKW))
                     buttonT.setIconSize(QSize(100,100)) 
+                    buttonT.setBID(self.z)
+                    buttonT.setType("king")
 
-    
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
@@ -141,7 +182,20 @@ class MainWindow(QMainWindow):
     def bClick(self):
         button = self.sender()
         buttonId = button.getBID()
-        print(f"Button {buttonId} clicked.")
+        buttonType = button.getType()
+        if(self.flag6 == True):
+            if(buttonId == self.stack[-1].getBID()+8 or 
+               buttonId == self.stack[-1].getBID()+9 or 
+               buttonId == self.stack[-1].getBID()+7):
+                button.setIcon(QIcon(self.absPPD))  
+                self.stack.pop().setIcon(QIcon())
+                self.flag6 = False
+        if(buttonType == "pawn" ):
+            self.flag6 = True
+            self.stack.append(button)
+        elif(buttonType == ""):
+            self.stack.append(button)
+
         
 ###############################################################################
 app = QApplication(sys.argv)
